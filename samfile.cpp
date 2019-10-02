464d850bf104193139e11f88bf854a65af95d3c9
David Deuber <ddeuber@student.ethz.ch>
2019-10-02 13:42:57 +0200

    added SAMFile, a class to manage to output into SAM format

---
+++ samfile.cpp
@@ -0,0 +1,107 @@
+#include "samfile.h"
+#include <iostream>
+
+SAMFile::SAMFile(std::string &fileName, std::string &refSeqName, int refSeqLength){
+	file.open(fileName);
+	file << "@HD\tN:1.4\tSO:unsorted\n";
+	file << "@SQ\tSN:" << refSeqName << "\tLN:" << refSeqLength << endline;
+	this->refSeqName = refSeqName;
+}
+
+void SAMFile::close(){
+	file.close();
+}
+	
+void SAMFile::add_paired_read_entry(std::string &readName, std::string &readSeq1, std::string &qualSeq1, int pos1, std::string &cigar1,
+													std::string &readSeq2, std::string &qualSeq2, int pos2, std::string &cigar2,
+													bool read1Reversed, int mapQuality1, int mapQuality2){
+	int tempLen;
+	if (read1Reversed) {
+		tempLen = pos1 - pos2 + readSeq1.length();
+		add_single_entry(readName, true, readSeq1, qualSeq1, true, pos1, cigar1, pos2, -tempLen, mapQuality1);
+		add_single_entry(readName, false, readSeq2, qualSeq2, false, pos2, cigar2, pos1, tempLen, mapQuality2);
+	} else {
+		tempLen = pos1 - pos1 + readSeq2.length();
+		add_single_entry(readName, true, readSeq1, qualSeq1, false, pos1, cigar1, pos2, tempLen, mapQuality1);
+		add_single_entry(readName, false, readSeq2, qualSeq2, true, pos2, cigar2, pos1, -tempLen, mapQuality2);	
+	}
+}
+
+void SAMFile::add_single_entry(std::string &readName, bool isRead1, std::string &readSeq, std::string &qualSeq, bool isReverse, int pos, std::string &cigar, int posNext,int tempLen, int mapQuality){
+	file << readName << tab;
+	file << generate_flag(pos, isReverse, isRead1, posNext) << tab; 
+	file << refSeqName << tab;
+	file << pos << tab;
+	file << mapQuality << tab;
+	file << cigar << tab;
+	file << "=" << tab;
+	file << posNext << tab;
+	file << tempLen << tab;
+	file << readSeq << tab;
+	file << qualSeq << endline;
+}
+
+int  SAMFile::generate_flag(int pos, bool isReverse, bool isRead1, int posOtherRead){
+	int  flag = 1;
+	
+	if (pos > 0 && posOtherRead > 0)
+		flag |= 2;
+	if (pos == 0)
+		flag |= 4;
+	if (posOtherRead == 0)
+		flag |= 8;
+
+	if (isReverse)
+		flag |= 16;
+	else
+		flag |= 32;
+
+	if (isRead1)
+		flag |= 64;
+	else
+		flag |= 128;
+		
+	return flag;
+}
+
+std::string SAMFile::alignment_to_CIGAR(std::string &dnaAligned, std::string &readAligned){
+	if (dnaAligned[0] != readAligned[0]){
+		std::cout << "First character of dnaAligned does not match the first character of readAligned! CIGAR will not be generated." << std::endl;
+		return "no cigar";
+	}
+
+	std::string cifar = "";
+	char lastmode = 'M'; //one of 'M' (match), 'I' (insert in reference), 'D' (deletion in reference), 'X' mismatch
+	char mode; 
+	int count = 1; 
+	char c1, c2;
+		
+	for (unsigned int i=1; i < dnaAligned.length(); ++i){
+		c1 = dnaAligned[i];
+		c2 = readAligned[i];
+		
+		if (c1 == c2)
+			mode = 'M';
+		else if (c1 == '-')
+			mode = 'I';
+		else if (c2 == '-')
+			mode = 'D';
+		else
+			mode = 'X';
+		
+		if (mode == lastmode) {
+			++count;
+		} else {
+			cifar += std::to_string(count);
+			cifar += lastmode;
+			lastmode = mode;
+			count = 1;
+		}
+	}
+
+	cifar += std::to_string(count);
+	cifar += lastmode;
+	return cifar;
+}
+
+
---
+++ samfile.h
@@ -0,0 +1,85 @@
+#ifndef SAMFILE_H
+#define SAMFILE_H
+
+#include <string>
+#include <fstream>
+
+class SAMFile {
+	private:
+		std::ofstream file;
+		std::string refSeqName;
+		std::string tab = "\t";
+		std::string endline = "\n";
+		
+		/*
+		 * The Following function adds the entry for one read to the file.
+		 * 
+		 * readName: name of read
+		 * isRead1: true if the read is read1, false for read2
+		 * readSeq: nucleotide sequence of read. IMPORTANT: if the read is a reverse read, readSeq needs to be already reversed and inverted
+		 * qualSeq: quality sequence of phred scores. IMPORTANT: if the read is a reverse read, qualSeq needs to be already reversed
+		 * isReverse: true if the read is a reverse read
+		 * pos: position of read in reference sequence. Note that the first nucleotide has position 1. If pos=0, this means that the read is unmapped.
+		 * cigar: CIGAR string 
+		 * posNext: pos of paired read
+		 * tempLen: length of segment from the first nucleotide of the forward read to the last nucleotide of the reverse read. 
+		 * 			The absolute value of tempLen is the same for both reads, but the reverse read tempLen has a negative sign. 
+		 * mapQuality = −10 log10 Pr{mapping position is wrong}, mapQuality=255 means that mapping quality is not available. default is 99
+		 *
+		 */
+		void add_single_entry(std::string &readName, bool isRead1, std::string &readSeq, std::string &qualSeq, bool isReverse, int pos, std::string &cigar, int posNext, int tempLen, int mapQuality=99); 
+
+		/*
+		 * Generates FLAG for a read.
+		 * 
+		 * pos: position of read in reference sequence. Note that the first nucleotide has position 1. If pos=0, this means that the read is unmapped.
+		 * isReverse: true if read is reverse read
+		 * isRead1: true if the read is read1, false for read2
+		 * posOtherRead: pos of the other read of the pair
+		*/	
+		int generate_flag(int pos, bool isReverse, bool isRead1, int posOtherRead);
+
+
+	public:
+		/*
+		 * Initializes SAMFile and prints header. 
+		 * 
+		 * fileName: desired name of the SAM file. Filetype (e.g. ".sam") is not added automatically but needs to be written in fileName.
+		 * refSeqName: name of the reference sequence
+		 * refSeqLength: legnth of the reference sequence
+		 */
+		SAMFile(std::string &fileName, std::string &refSeqName, int refSeqLength);
+
+		/*
+		 * File needs to be closed in order to flush the changes to disk.
+		 */
+		void close();
+
+		/*
+		 * The Following function adds two lines to the SAM file, one for each of the two reads. 
+		 * 
+		 * readName: name of read
+		 * readSeq1: nucleotide sequence of read1. IMPORTANT: if read1 is a reverse read, readSeq1 needs to be already reversed and inverted
+		 * qualSeq1: quality sequence of phred scores of read1. IMPORTANT: if read1  is a reverse read, qualSeq1 needs to be already reversed
+		 * pos1: position of read1 in reference sequence. Note that the first nucleotide has position 1. If pos=0, this means that the read is unmapped.
+		 * cigar1: CIGAR string of read1
+		 * readSeq2: nucleotide sequence of read2. IMPORTANT: if read2 is a reverse read, readSeq2 needs to be already reversed and inverted
+		 * qualSeq2: quality sequence of phred scores of read2. IMPORTANT: if read2  is a reverse read, qualSeq2 needs to be already reversed
+		 * pos2: position of read2 in reference sequence. Note that the first nucleotide has position 1. If pos=0, this means that the read is unmapped.
+		 * cigar2: CIGAR string of read2	
+		 * read1Reversed: true if read1 is the reverse read, false if read2 is the reverse read 
+		 * mapQuality1/2 = −10 log10 Pr{mapping position is wrong} of read1/2, mapQuality=255 means that mapping quality is not available. default is 99
+		 *
+		 */
+		void add_paired_read_entry(std::string &readName, std::string &readSeq1, std::string &qaulSeq1, int pos1, std::string &cigar1,
+													std::string &readSeq2, std::string &qualSeq2, int pos2, std::string &cigar2,
+													bool read1Reversed, int mapQuality1=99, int mapQuality2=99);
+
+		/*
+		 * Converts a global alignment between a part of the reference sequence (dnaAligned) and the read (readAligned) into a CIGAR string. 
+		 * IMPORTANT: The first character has to be a match and must correspond to the position of the read in the dna. Otherwise the position of the read might be wrong.
+		 */	
+		static std::string alignment_to_CIGAR(std::string &dnaAligned, std::string &readAligned);
+};
+
+#endif
