97c515e38fcd8c161cdba8ccd22160c44ad05fb5
Sotiris Anagnostidis <sot.anagn@gmail.com>
2019-10-11 12:09:09 +0200

    Ukkonen's algorithm self implementation

--- SuffixTree.cpp
+++ SuffixTree.cpp
@@ -1,147 +1,195 @@
-// Copyright (c) 2012 Adam Serafini
-// https://github.com/adamserafini/suffix-tree/blob/master/src/SuffixTree.cpp
-
-#include <map>
-#include <sstream>
 #include <iostream>
 #include <string>
-#include <vector>
+#include <map>
 
 #include "SuffixTree.h"
-#include "Node.h"
-#include "Suffix.h"
+#include "bwt.h"
 
-SuffixTree::SuffixTree() {
-  // Internal node IDs start at zero and decrement. For example, the root node,
-  // which can be considered the first internal node has an ID of 0. The next
-  // internal node has an ID of -1, followed by -2 and so forth.
+using namespace std;
 
-  // While not neccessary for the algorithm to function, each node having a
-  // unique ID is important when using Graphiz to visualize the structure.
-  internal_node_ID = 0;
+// std::map<char, int> mapOfGenes = {{'$', 0}, {'A', 1}, {'C', 2}, {'G', 3}, {'T', 4}};
 
-  current_end = new int(0);
-  root = new Node(NULL, 1, current_end, internal_node_ID);
-}
+void SuffixTree::build(){
+    // initializations
+    current_end = new int[1];
+    *(current_end) = 0;
+    root = new SuffixNode(1, current_end);
+    root->internalNode = true;
+    
+    remaining = 0;
+    activeNode = root;
+    activeLength = 0;
+    activeEdge = -1;
 
-void SuffixTree::construct(std::string s) {
-  length = s.length();
-  tree_string = s;
-
-  // Construct Implicit Tree I(1).
-  (*current_end)++;
-  last_leaf_extension = new Node(root, 1, current_end, 1);
-  root->add_child(*this, last_leaf_extension);
-
-  for (int i = 1; i < length; i++)
-    SPA(i);
-}
-
-// SPA: Single Phase Algorithm (Gusfield, 1997)
-void SuffixTree::SPA(int i) {
-  // Do phase i + 1.
-
-  Suffix previous_suffix(last_leaf_extension, *current_end);
-
-  // Increment the current_end pointer: this implicitly applies Rule 1 to all
-  // leaf edges in the tree.
-  (*current_end)++;
-
-  // Explicitly compute successive extensions starting at j(i) + 1 where (i)
-  // is the ID of the last leaf extension from the previous phase.
-  for (int j = (last_leaf_extension->ID + 1); j <= i + 1; j++) {
-    Rule rule_applied = SEA(previous_suffix, j, i);
-    if (rule_applied == RULE_3)
-      break;
-  }
-}
-
-// SEA: Single Extension Algorithm (Gusfield, 1997)
-SuffixTree::Rule SuffixTree::SEA(Suffix& previous_suffix, int j, int i) {
-  int begin_index, end_index;
-  Node* origin = previous_suffix.walk_up(begin_index, end_index);
-  Suffix suffix = (origin == root ? get_suffix(root, j, i)
-    : get_suffix(origin->suffix_link, begin_index, end_index));
-
-  Rule rule_applied;
-  if (suffix.RULE2_conditions(*this, i + 1)) {
-    RULE2(suffix, i + 1, j);
-    rule_applied = RULE_2;
-  } else {
-    rule_applied = RULE_3;
-  }
-
-  if (previous_suffix.new_internal_node)
-    previous_suffix.node->suffix_link = suffix.node;
-
-  previous_suffix = suffix;
-  return rule_applied;
-}
-
-// The 'skip/count' trick for suffix tree traversal (Gusfield, 1997)
-Suffix SuffixTree::get_suffix(Node* origin, int begin_index, int end_index) {
-  int char_index = *origin->end_index;
-
-  while (begin_index <= end_index) {
-    origin = origin->get_child(*this, begin_index);
-    if (origin->edge_length() < end_index - begin_index + 1)
-      char_index = *origin->end_index;
-    else
-      char_index = origin->begin_index + (end_index - begin_index);
-    begin_index+=origin->edge_length();
-  }
-  return Suffix(origin, char_index);
-}
-
-std::string SuffixTree::get_substr(int start_pos, int end_pos) {
-  if (start_pos > end_pos) return std::string();
-  // This is 1-indexed to match the algorithm's original description in the
-  // paper. For example, "foobar".get_substr(2, 4) == "oob".
-  return tree_string.substr(start_pos - 1, end_pos - start_pos + 1);
-}
-
-char SuffixTree::get_char_at_index(int index) const {
-  // Also 1-indexed. For example, "foobar".get_char_at_index(4) == 'b'
-  return tree_string[index - 1];
-}
-
-void SuffixTree::RULE2(Suffix& suffix, int char_index, int new_leaf_ID) {
-  if (!suffix.ends_at_node()) {  // eg. in case 2 (path ends inside an edge)
-    suffix.node->split_edge(*this, suffix.char_index, --internal_node_ID);
-    suffix.node = suffix.node->parent;
-    suffix.new_internal_node = true;
-  }
-  Node* new_leaf = new Node(suffix.node, char_index, current_end, new_leaf_ID);
-  suffix.node->add_child(*this, new_leaf);
-  last_leaf_extension = new_leaf;
-}
-
-std::string SuffixTree::compute_transformation(int* location_array) {
-  this->tree_string_size = tree_string.length();
-  char* transformation = new char[tree_string_size + 1];
-  transformation[tree_string_size] = 0;
-
-  unsigned long* pos = new unsigned long;
-  *pos = tree_string_size - 1;
-
-  compute_transformation_node(root, transformation, pos, location_array);
-
-  return std::string(transformation);
-}
-
-void SuffixTree::compute_transformation_node(Node* parent, char* transformation, unsigned long* pos, int* location_array) {
-  std::map<int, Node*>::iterator it = parent->children.begin();
-
-  for (; it != parent->children.end(); it++) {
-    // Child nodes are stored on the parent node in a map of integers
-    // (it->first) to Node pointers (it->second).
-    Node* child_node = it->second;
-    if (child_node->ID > 0) {
-        /* std::cout << child_node->ID << ' ' << child_node->begin_index << ' ' << *(child_node->end_index) << ' ' << tree_string[(child_node->ID + tree_string_size - 2) % tree_string_size] << std::endl; */
-        transformation[*pos] = tree_string[(child_node->ID + tree_string_size - 2) % tree_string_size];
-        location_array[*pos] = child_node->ID - 1;
-        *pos = *pos - 1;
+    for(unsigned long i = 0; i < str.length(); ++i){
+        addSuffix(i);
     }
-    compute_transformation_node(child_node, transformation, pos, location_array);
-  }
+}
+
+int SuffixTree::tryNextCharacter(int i){
+    // returns the next character if exists in the tree or -1 for the creation of a new edge
+    SuffixNode* node = selectNode();
+
+    int lengthOfEdge = edgeLength(node);
+    
+    if (lengthOfEdge >= activeLength){
+        return str[activeNode->children[mapOfGenes[str[activeEdge]]]->start + activeLength];
+    }
+
+    if (lengthOfEdge + 1 == activeLength){
+        // node exists at this specific point
+        if (node->children[mapOfGenes[str[i]]] != nullptr){
+            return str[i];
+        }
+        return -1;
+    } else{
+        // traverse over node
+        activeNode = node;
+        activeLength = activeLength - lengthOfEdge - 1;
+        activeEdge = activeEdge + lengthOfEdge + 1;
+
+        return tryNextCharacter(i);
+    }
+}
+
+void SuffixTree::walkDown(int i){
+    SuffixNode* node = selectNode();
+
+    if (edgeLength(node) < activeLength){
+        activeNode = node;
+        activeLength = activeLength - edgeLength(node);
+        activeEdge = node->children[mapOfGenes[str[i]]]->start;
+    } else{
+        activeLength ++;
+    }
+}
+
+
+SuffixNode* SuffixTree::selectNode() {
+    return activeNode->children[mapOfGenes[str[activeEdge]]]; 
+}
+
+SuffixNode* SuffixTree::selectNode(int index) { 
+    return activeNode->children[mapOfGenes[str[index]]]; 
+}
+
+void SuffixTree::addSuffix(int i){
+    // cout << "i: " << i << " activeLength: " << activeLength << endl;
+    SuffixNode* lastCreated = nullptr;
+
+    *(current_end) = *(current_end) + 1;
+
+    remaining ++;
+
+    while (remaining > 0){
+        // cout << "remaining: " << remaining << endl;
+        if (activeLength == 0){
+            // we are at root
+            SuffixNode* node = selectNode(i);
+            if (node != nullptr){
+                // RULE 3 extension
+                activeEdge = node->start;
+                activeLength ++;
+                break;
+            } else {
+                // cout << "RULE2 upper" << endl;
+                // RULE 2
+                root->children[mapOfGenes[str[i]]] = new SuffixNode(i, current_end);
+                remaining --;
+            }
+        } else {
+            int nextChar = tryNextCharacter(i);
+            // cout << "nextChar: " << nextChar << endl;
+
+            if (char(nextChar) == str[i]){
+                // cout << "Found it" << endl;
+                if (lastCreated != nullptr)
+                    lastCreated->suffixLink = selectNode();
+
+                walkDown(i);
+                break;
+            } else if (nextChar < 0){
+                SuffixNode* node = selectNode();
+                node->children[mapOfGenes[str[i]]] = new SuffixNode(i, current_end);
+
+                if (lastCreated != nullptr)
+                    lastCreated->suffixLink = node;
+
+                lastCreated = node;
+
+                if (activeNode != root)
+                    activeNode = activeNode->suffixLink;
+                else {
+                    activeEdge ++;
+                    activeLength --;
+                }
+
+                remaining --;
+            } else{
+                // cout << "Adding internal node.." << endl;
+                // RULE 2
+                SuffixNode* node = selectNode();
+                int oldStart = node->start;
+                node->start = node->start + activeLength;
+
+                int* thisEnd = new int[1];
+                *(thisEnd) = oldStart + activeLength - 1;
+                SuffixNode* newInternalNode = new SuffixNode(oldStart, thisEnd);
+
+                SuffixNode* newLeafNode = new SuffixNode(i, current_end);
+
+                newInternalNode->children[mapOfGenes[str[newInternalNode->start + activeLength]]] = node;
+                newInternalNode->children[mapOfGenes[str[i]]] = newLeafNode;
+                newInternalNode->internalNode = true;
+                activeNode->children[mapOfGenes[str[newInternalNode->start]]] = newInternalNode;
+
+                if (lastCreated != nullptr)
+                    lastCreated->suffixLink = newInternalNode;
+
+                lastCreated = newInternalNode;
+                newInternalNode->suffixLink = root;
+
+                if (activeNode != root)
+                    activeNode = activeNode->suffixLink;
+                else{
+                    activeEdge ++;
+                    activeLength --;
+                }
+                remaining --;
+            }
+        }
+    }
+}
+
+
+
+string SuffixTree::bwt(int* location_array){
+    unsigned long str_len = str.length();
+
+    char* transformation = new char[str_len + 1];
+    unsigned long* pos = new unsigned long;
+    *pos = 0;
+
+    bwt(root, 0, str_len, pos, transformation, location_array);
+
+    return string(transformation);
+}
+
+void SuffixTree::bwt(SuffixNode* node, int val, int size, unsigned long* pos, char* transformation, int* location_array){
+    if (node == nullptr)
+        return;
+
+    if (!node->internalNode) {
+        // cout << "Added: " << transformation[*pos] << " for position " << (size - (*(node->end) - node->start + val) - 1 + size) % size << endl;
+        transformation[*pos] = str[(size - (*(node->end) - node->start + val) - 1 + size) % size];
+        location_array[*pos] = node->start - val;
+
+        (*pos) = (*pos) + 1;
+        return;
+    } else
+        if (node != root)
+            val += *(node->end) - node->start + 1;
+
+    for(int i = 0; i < 5; ++i)
+        bwt(node->children[i], val, size, pos, transformation, location_array);
 }
